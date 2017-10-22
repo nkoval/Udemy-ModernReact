e53c93b377106488437bdf70054d17b034c029c8
Mykola Koval <mykola.koval@booking.com>
2017-10-22 13:04:07 +0200

    Search and video select

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,16 +9,17 @@ class SearchBar extends Component {
 
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input
                     value = {this.state.term}
-                    onChange = {this.onInputChange.bind(this)}/>
+                    onChange = {event => this.onInputChange(event.target.value)}/>
             </div>
         );
     }
 
-    onInputChange(event) {
-        this.setState({term: event.target.value})
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 }
 export default SearchBar
--- src/index.js
+++ src/index.js
@@ -12,12 +12,7 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-            })
-        });
+        this.videoSearch('surfboard');
 
         this.state = {
             videos: [],
@@ -25,10 +20,19 @@ class App extends Component {
         };
     }
 
+    videoSearch (term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            })
+        });
+    }
+
     render() {
         return (
             <div>
-                <SearchBar/>
+                <SearchBar onSearchTermChange = {term => this.videoSearch(term)}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
